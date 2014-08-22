2a985b190f374b76791fb76e8afb848fa4c3d2fa
jffjs <jffreyjs@gmail.com>
2014-08-22 16:55:04 -0400

    add asteroidGroup to replace factory

---
+++ src/game/entities/asteroidGroup.js
@@ -0,0 +1,37 @@
+'use strict';
+
+var Asteroid = require('../entities/asteroid');
+
+var randBetween = function(min, max) {
+  return Math.floor(Math.random() * ((max + 1) - min)) + min;
+};
+
+var AsteroidGroup = function(game) {
+  this.game = game;
+  this.MAX_ASTEROIDS = 10;
+  this.MIN_SPEED = 20,
+  this.MAX_SPEED = 150,
+  this.MIN_ROTATION = 20,
+  this.MAX_ROTATION = 100,
+  this.COLORS = ['brown', 'grey'],
+  this.SIZES = ['big', 'medium', 'small'];
+  this.pool = null;
+};
+
+AsteroidGroup.prototype.create = function() {
+  this.pool = this.game.add.group();
+  for(var i = 0; i < this.MAX_ASTEROIDS; i++) {
+    var start = new Phaser.Point(randBetween(0, game.width), randBetween(0, game.height));
+    var velocity = new Phaser.Point(randBetween(this.MIN_SPEED, this.MAX_SPEED) * Phaser.Math.randomSign(),
+                                    randBetween(this.MIN_SPEED, this.MAX_SPEED) * Phaser.Math.randomSign());
+    var rotation = randBetween(this.MIN_ROTATION, this.MAX_ROTATION);
+    var color = COLORS[randBetween(0, this.COLORS.length - 1)];
+    var size = SIZES[randBetween(0, this.SIZES.length - 1)];
+    var asteroid = new Asteroid(game, start, velocity, rotation, color, size);
+    this.pool.add(asteroid.sprite);
+  }
+
+  return this;
+};
+
+module.exports = AsteroidGroup;
